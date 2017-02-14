e2d845c5a04730644fc77f6bcfe10031000b0e92
jenilee <jenilee.hsu@gmail.com>
2017-02-13 20:15:07 -0800

    added index and show methods

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,8 +1,20 @@
 class TeamsController < ApplicationController
 
+  def index
+    @teams = Unirest.get("http://localhost:3000/api/v2/teams").body
+  end
+
   def show
     id = params[:id]
     @team = Unirest.get("http://localhost:3000/api/v2/teams/#{id}").body
+  end
+
+  def create
+    @team = Team.create(
+      state: params[:state],
+      creature: params[:creature],
+      sport: params[:sport]
+      )
   end
 
 end
